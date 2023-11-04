9ae2dd1f9d05913d29e7d87ab5e933a0b8ddde8c
Irene Boby <ireneboby.dev@gmail.com>
2023-11-04 18:53:57 -0400

    more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeListComponent } from './home-list/home-list.component';
 import { NewListComponent } from './new-list/new-list.component';
-
+import { RoutesComponent } from './routes/routes.component';
 const routes: Routes = [
   { path: 'home-list', component: HomeListComponent },
   { path: 'new-list', component: NewListComponent },
+  { path: 'routes', component: RoutesComponent },
   { path: '', redirectTo: '/home-list', pathMatch: 'full' },
 ];
 
